6318b680c432fd931a1fa2f87259982f60711698
schawanji <schawanji@gmail.com>
2023-12-03 12:34:43 +0100

    Updated url.py

--- src/worldbank/urls.py
+++ src/worldbank/urls.py
@@ -4,3 +4,18 @@ from .views import mapView
 urlpatterns = [
   url('dashboard/', mapView, name='dashboard'),
 ]
+
+from django.conf.urls import url,include
+from geonode.api.urls import router
+from django.urls import path
+from .views import GeoJSONAPIView
+from .views import mapView ,geojsonapi
+
+#router.register(r'mapView')
+
+urlpatterns = [
+   url('dashboard/', mapView, name='dashboard'),
+   url('geojsonapi/',geojsonapi,name='geojsonapi'),
+   path('geojsonapi_view/', GeoJSONAPIView.as_view(), name='geojsonapi'),
+   
+]
