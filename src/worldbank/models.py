aecfa96c413629c341267b57dcdf8279ecaaa1d5
schawanji <schawanji@gmail.com>
2023-12-03 10:44:39 +0100

    Updated Models

--- src/worldbank/models.py
+++ src/worldbank/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from geonode.layers.models import Dataset
+from geonode.layers.models import Attribute
+from geonode.layers.models import Style
+from geonode.maps.models import Map,MapLayer
 # Create your models here.
+class Layer(models.Model):
+    """
+    A collection is a set of resources linked to a GeoNode group
+    """
+    layer =models.ForeignKey(MapLayer,related_name='map_layers',on_delete=models.CASCADE,null=True)
+
+    
+    
--- src/worldbank/views.py
+++ src/worldbank/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from geonode.upload.models import Upload
+from django.http import HttpResponse
+from osgeo import ogr
+import json
+import geopandas as gpd
+from django.http import JsonResponse
+from django.views import View
+from django.urls import reverse
 # Create your views here.
 
 
 def mapView(request):
     # Your view's logic here
     return render(request, 'worldbank/map.html')
+
+def geojsonapi(request):
+    file_object=Upload.objects.all()
+    #file_object=Upload.objects.get(pk=58)
+    ext ='.shp'
+    path=file_object.upload_dir+'/'
+    filename=file_object.name 
+    file_full_path= path+filename+ext
+    shp_path = file_full_path
+    print(shp_path)
+    #Using geopandas
+    gdf = gpd.read_file(file_full_path)
+    json_data = json.loads(gdf.to_json())
+    return JsonResponse(json_data, safe=False)
