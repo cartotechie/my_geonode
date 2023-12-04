57e2984f033066dc91380e689f372e42211b497b
schawanji <schawanji@gmail.com>
2023-12-04 21:14:57 +0100

    Updated mapjs

--- src/worldbank/src/map.js
+++ src/worldbank/src/map.js
@@ -36,7 +36,8 @@ const getLayers = async () => {
       const responseJSON = await response.json();
       const layer = new VectorLayer({
         style:function(feature) {
-          return getPolygonStyle(feature)
+          //return getPolygonStyle(feature)
+          return adminBoundaryStyle
         },
         source: new VectorSource({
           features: new GeoJSON().readFeatures(responseJSON),
@@ -47,18 +48,23 @@ const getLayers = async () => {
       });
       mapLayers.push(layer);
       console.log(responseJSON);
+      
     }
 
     if (response1.ok) {
       const responseJSON = await response1.json();
+      console.log(responseJSON);
       const layer = createVectorLayer(responseJSON, 'Layer2',substationStyle);
       mapLayers.push(layer);
+      
     }
 
     if (response2.ok) {
       const responseJSON = await response2.json();
+      console.log(responseJSON);
       const layer = createVectorLayer(responseJSON, 'Layer3',powerLineStyle);
       mapLayers.push(layer);
+      
     }
 
     if (mapLayers.length > 0) {
@@ -71,23 +77,23 @@ const getLayers = async () => {
         layers: mapLayers
       }));
 
-      const layer =  mapLayers[0]
-      //console.log('layer.getSource().getFeatures()')
+      const layer =  mapLayers[1]
+      console.log(layer.getSource().getFeatures())
        ///populate province dropdown menu
              for(let i = 0;i < getProvinceName(layer.getSource().getFeatures()).length;i++ ){
               //console.log((layer.getSource().getFeatures())[i])
               //console.log(getProvinceName(layer.getSource().getFeatures())[i])
-               populateOptionElemets(getProvinceName(layer.getSource().getFeatures())[i], "provinces")}
+               populateOptionElemets(getProvinceName(layer.getSource().getFeatures())[i], "division")}
 
                  // Select features from options dropdown arrow
 			function onchange(event) {
 				event.preventDefault()
 				selectedProvince = event.target.value;
 				//console.log(selectedProvince)
-				for (let i = 0; document.getElementById("provinces").options.length > i; i++) {
+				for (let i = 0; document.getElementById("division").options.length > i; i++) {
 					//console.log(document.getElementById("provinces").options[i].value)
 					if (selectedProvince === event.target.value) {
-						document.querySelector(`#provinces option[value= "${document.getElementById("provinces").options[i].value}"]`).disabled = true
+						document.querySelector(`#provinces option[value= "${document.getElementById("division").options[i].value}"]`).disabled = true
 					}
 				}
 				/*********************** */
@@ -109,7 +115,80 @@ const getLayers = async () => {
 				
 			}
 			document.getElementById('provinces').addEventListener('change', onchange)
-         
+      map.on('click', function (event) {
+        var coordinate = event.coordinate;
+        console.log('Clicked coordinate:', coordinate);
+    alert('HELLO')
+        // Open the info box and display information
+        var infoBox = document.getElementById('info-box');
+        infoBox.innerHTML = 'Latitude: ' + coordinate[1] + '<br>Longitude: ' + coordinate[0];
+        infoBox.style.left = event.pixel[0] + 'px';
+        infoBox.style.top = event.pixel[1] + 'px';
+        infoBox.style.display = 'block';
+
+        
+      });
+
+// Sample data for each chart
+var lineChartData = {
+  labels: ['January', 'February', 'March', 'April', 'May'],
+  datasets: [{
+      label: 'Line Chart',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      data: [65, 59, 80, 81, 56],
+      fill: false,
+  }]
+};
+
+var barChartData = {
+  labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'],
+  datasets: [{
+      label: 'Bar Chart',
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      borderWidth: 1,
+      data: [65, 59, 80, 81, 56],
+  }]
+};
+
+var bar2ChartData = {
+  labels: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'],
+  datasets: [{
+      label: 'Bar Chart',
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      borderWidth: 1,
+      data: [65, 59, 80, 81, 56],
+  }]
+};
+
+var radarChartData = {
+  labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+  datasets: [{
+      label: 'Radar Chart',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      borderWidth: 1,
+      data: [65, 59, 90, 81, 56, 55, 40],
+  }]
+};
+
+// Function to create a chart
+function createChart(chartId, chartType, data) {
+  var ctx = document.getElementById(chartId).getContext('2d');
+  return new Chart(ctx, {
+      type: chartType,
+      data: data,
+      options: {} // You can customize options here
+  });
+}
+
+// Create charts using the above data
+var lineChart = createChart('graph1', 'line', lineChartData);
+var barChart = createChart('graph2', 'bar', barChartData);
+var bar2Chart = createChart('graph3', 'bar', barChartData);
+var radarChart = createChart('graph4', 'radar', radarChartData);
+
     } else {
       throw new Error('No valid GeoJSON data available');
     }
@@ -120,3 +199,8 @@ const getLayers = async () => {
 };
 
 getLayers();
+
+
+/*****************************************+ */
+
+ 
